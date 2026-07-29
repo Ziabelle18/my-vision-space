e67733f4f3d63e3dc44084048f9e0c3464618819
ZD <krizzialester@gmail.com>
2026-07-30 01:48:18 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -53,8 +53,8 @@ try {
   console.warn('Boardly background messaging is not supported here:', error);
 }
 
-const BOARDLY_CACHE = 'boardly-shell-v53-social-chat';
-const BOARDLY_VERSION = 'v53';
+const BOARDLY_CACHE = 'boardly-shell-v54-comments-wiggle';
+const BOARDLY_VERSION = 'v54';
 const BOARDLY_SHELL = [
   './',
   './index.html',
