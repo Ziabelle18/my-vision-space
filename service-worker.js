b0e6432f85858b0e4eec4f5ed4157228e291eccd
ZD <krizzialester@gmail.com>
2026-07-29 18:16:23 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const BOARDLY_CACHE = 'boardly-shell-v37-visible-chat-wiggle';
-const BOARDLY_VERSION = 'v37';
+const BOARDLY_CACHE = 'boardly-shell-v38-chat-animation-timing';
+const BOARDLY_VERSION = 'v38';
 const BOARDLY_SHELL = [
   './',
   './index.html',
