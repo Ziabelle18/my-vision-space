9c6787156a7a118f8a23f986f02fbfd649199301
ZD <krizzialester@gmail.com>
2026-07-30 00:07:24 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -51,8 +51,8 @@ try {
   console.warn('Boardly background messaging is not supported here:', error);
 }
 
-const BOARDLY_CACHE = 'boardly-shell-v48-polaroid-styles';
-const BOARDLY_VERSION = 'v48';
+const BOARDLY_CACHE = 'boardly-shell-v49-optional-captions';
+const BOARDLY_VERSION = 'v49';
 const BOARDLY_SHELL = [
   './',
   './index.html',
